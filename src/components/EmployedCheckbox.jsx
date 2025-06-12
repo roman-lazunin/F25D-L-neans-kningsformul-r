bf669bd40242fa1496e0ade95cb6d679b94da9c5
“Roman <lazunin.roman@gmail.com>
2025-06-12 12:32:58 +0200

    enployment style update

--- src/components/EmployedCheckbox.jsx
+++ src/components/EmployedCheckbox.jsx
@@ -3,8 +3,14 @@ import '../styles/components/EmployedCheckbox.css';
 
 const EmployedCheckbox = ({ checked, onChange }) => (
   <div className={`employed-checkbox ${checked ? 'checked' : ''}`}>
-    <label>
-      <input type="checkbox" name="employed" checked={checked} onChange={onChange} />
+    <label htmlFor="employed-input">
+      <input 
+        id="employed-input"
+        type="checkbox" 
+        name="employed" 
+        checked={checked} 
+        onChange={onChange} 
+      />
       <span className="checkbox-label-text">Är du anställd?</span>
     </label>
   </div>
